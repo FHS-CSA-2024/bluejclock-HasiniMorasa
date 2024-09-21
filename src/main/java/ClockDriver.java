ca510bef213f7776957063b9c08249510a9c8901
HasiniMorasa <hmorasa9@gmail.com>
2024-09-20 22:25:59 -0700

    Update ClockDriver.java

--- src/main/java/ClockDriver.java
+++ src/main/java/ClockDriver.java
@@ -1,3 +1,5 @@
+package src.main.java;
+
 
 public class ClockDriver
 {
@@ -14,7 +16,26 @@ public class ClockDriver
         //  * Tick test for 01:59 to 02:00
         //  * Tick test for 09:59 to 10:00
         //  * Tick test for 23:59 to 00:00
+        System.out.println("Testing Clock Display");
+        ClockDisplay test1 = new ClockDisplay();
+        String test1Output = test1.getTime();
+        System.out.println("\tEmpty Con. Test - " + test1Output);
         
+        ClockDisplay test2 = new ClockDisplay(2,45);
+        String test2Output = test2.getTime();
+        System.out.println("\t2 Arg con.Teat - " +test2Output);
+        
+        test2.setTime(5,15);
+        
+        String test3output = test2.getTime();
+        System.out.println("\t set Time Test - " + test3output); 
+        
+        ClockDisplay tickTest1 = new ClockDisplay(3,23);
+        String tickTest1Output = tickTest1.getTime();
+        System.out.println("\tTickTest1 Before increment/ticking -" + tickTest1Output);
+        tickTest1.timeTick();
+        String AfterTickTest1Output = tickTest1.getTime();
+        System.out.println("\tTickTest1 After increment -" + AfterTickTest1Output);
         
         //Implement tests for your ClockDisplaySeconds
         //  * Test that you can do the basics
@@ -28,17 +49,12 @@ public class ClockDriver
         //  * Tick test for 01:59:59 to 02:00:00
         //  * Tick test for 23:59:59 to 00:00:00
         
-        /*CHALLENGE*/
-        //Implement tests for your ClockDisplay12Hour
-        //  * Test that you can do the basics
-        //      * Empty constructor & read time
-        //      * Constructor w/ time given & read time
-        //      * Set time
-        //      * Read time
-        //      * Tick time
-        //  * Tick test for 03:32PM to 03:33PM
-        //  * Tick test for 11:59PM to 12:00AM
-        //  * Tick test for 11:59AM to 12:00PM
-        //  * Tick test for 12:59PM to 01:00PM
+        System.out.println("Testing ClockDisplaySeconds Display");
+        ClockDisplaySeconds ClockDisplaySecondstest1 = new ClockDisplaySeconds(); // Empty constructor & read time
+        String ClockDisplaySecondstest1Output = ClockDisplaySecondstest1.getTime(); //& read time
+        System.out.println("\tEmpty Con. ClockDisplaySeconds - " + ClockDisplaySecondstest1Output);
+        
+        
+        
     }
 }
